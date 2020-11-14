1c5de66d5c728630f268505ed5d9e297d128d627
Tamara A <akira-49-q@yandex.ru>
2020-11-14 17:54:27 +0200

    fix: исправить ссылку у лого

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header(props) {
   /** Разметка хедера */
   return (
     <header className="header page__header page__section">
-      <a href="https://akr-tamara-a.github.io/mesto/index.html" className="header__logo-link">
+      <a href="https://akr-tamara-a.github.io/react-mesto-auth/" className="header__logo-link">
         <img src={logoPath} alt="logo" className="logo header__logo" />
       </a>
       <h1 className="header__title">Сервис Mesto Russia</h1>
